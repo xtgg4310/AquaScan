ed02e58b7455fb4b8e6dad0b1a3b2be2000bd060
Enigma <626201515@qq.com>
2026-03-20 21:01:54 +0800

    Remove select_detect_model function
    Removed the select_detect_model function for model selection.

--- model/__init__.py
+++ model/__init__.py
@@ -11,14 +11,6 @@ def select_model(args):
         raise NotImplementedError
     return model
 
-def select_detect_model(args):
-    if args.model_detect_type == 'res18_linear':
-        model = resnet_linear(in_channel=args.channel_detect_num, num_classes=args.num_detect_classes, name='resnet18')
-    else:
-        raise NotImplementedError
-    return model
-
-
 def equip_multi_gpu(model, args):
     model = nn.DataParallel(model, device_ids=args.gpus)
     return model
